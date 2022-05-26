4ae43f0f7fdd560b0d9c4bf97b1ceb3ed73056a7
Billy <bichacon98s@gmail.com>
2022-05-26 00:18:48 -0600

    Update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CarreraController;
 use App\Http\Controllers\ProfesorController;
 use App\Http\Controllers\CursosController;
 use App\Http\Controllers\AlumnoController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CicloController;
 use App\Http\Controllers\OfertaController;
 use App\Http\Controllers\MatriculaController;
@@ -13,8 +14,9 @@ use App\Http\Controllers\GrupoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
 
 Route::get('/users', [UsersController::class, 'index']);
 
